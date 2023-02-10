a1c43579f3c4e1391e5f1f3383818ccae48e3b2c
Madhav Gautam <madhavgautam2016@gmail.com>
2023-02-10 21:07:27 +0545

    initial commit with socket setup

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,53 @@
+import {
+    MessageBody,
+    SubscribeMessage,
+    WebSocketGateway,
+    WebSocketServer,
+    WsResponse,
+    ConnectedSocket
+} from '@nestjs/websockets';
+import { from, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+    cors: {
+        origin: '*',
+    },
+})
+
+export class EventsGateway {
+    @WebSocketServer()
+    server: Server;
+
+    // @SubscribeMessage('events')
+    // findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    //     return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+    // }
+
+    // @SubscribeMessage('identity')
+    // async identity(@MessageBody() data: number): Promise<number> {
+    //     return data;
+    // }
+
+    // @SubscribeMessage('createRoom')
+    // async createRoom(@MessageBody() data: any, @ConnectedSocket() client: Socket): Promise<void> {
+    //     client.join(data.roomName);
+    //     await this.broadcastToRoom(data.roomName, "newuser", data);
+    // }
+
+    @SubscribeMessage('joinRoom')
+    async joinRoom(@MessageBody() roomName: string, @ConnectedSocket() client: Socket): Promise<void> {
+        client.join(roomName);
+        await this.broadcastToRoom(roomName, "newuser", "New user Joined");
+    }
+
+    @SubscribeMessage('shareContent')
+    async shareToAll(@MessageBody() data: any, @ConnectedSocket() client: Socket): Promise<void> {
+        await this.broadcastToRoom(data.roomName, "newcontent", data);
+    }
+
+    private async broadcastToRoom(roomName: string, event: string, message: any): Promise<void> {
+        this.server.to(roomName).emit(event, message);
+    }
+}
